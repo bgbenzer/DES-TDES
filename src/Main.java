fac247a1e898351e972eb62249f5b887e26a379d
b21945906 <b21945906@cs.hacettepe.edu.tr>
2022-11-02 15:32:14 +0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -65,7 +65,20 @@ public class Main {
             long finishTime = System.currentTimeMillis();
             long timeDiff = finishTime-startTime;
 
-            Operations.writeAsByte(encryptedOrDecrypted, args[5]);
+            if(args[1].equals("-e")){
+                    Operations.writeAsByte(encryptedOrDecrypted, args[5]);
+            }
+            else if(args[1].equals("-d")){
+                char[] finalData =  new String(encryptedOrDecrypted).toCharArray();
+                String finalString = "";
+                for(char c: finalData){
+                    if(c != '\u0000'){
+                        finalString = finalString+c;
+                    }
+                }
+                Operations.writeToFile(finalString, args[5]);
+            }
+
             Operations.writeToFile(args[3]+ " "+ args[5]+ " "+ ((args[1].equals("-e"))? "enc": "dec")+" "+args[6]+" "+args[7]+ " "+ timeDiff ,"run.log");
 
         }
@@ -109,7 +122,19 @@ public class Main {
             }
             long finishTime = System.currentTimeMillis();
             long timeDiff = finishTime-startTime;
-            Operations.writeAsByte(encryptedOrDecrypted, args[5]);
+            if(args[1].equals("-e")){
+                Operations.writeAsByte(encryptedOrDecrypted, args[5]);
+            }
+            else if(args[1].equals("-d")){
+                char[] finalData =  new String(encryptedOrDecrypted).toCharArray();
+                String finalString = "";
+                for(char c: finalData){
+                    if(c != '\u0000'){
+                        finalString = finalString+c;
+                    }
+                }
+                Operations.writeToFile(finalString, args[5]);
+            }
             Operations.writeToFile(args[3]+ " "+ args[5]+ " "+ ((args[1].equals("-e"))? "enc": "dec")+" "+args[6]+" "+args[7]+ " "+ timeDiff ,"run.log");
 
 
